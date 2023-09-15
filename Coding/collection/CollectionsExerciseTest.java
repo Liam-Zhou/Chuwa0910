361eed36bea3fb1d221a2dd3bba27892bc743bba
Huixuan Wang <whx987172@gmail.com>
2023-09-15 16:47:21 -0400

    hw3 finish collection

---
+++ Coding/collection/CollectionsExerciseTest.java
@@ -0,0 +1,51 @@
+package com.chuwa.exercise.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author b1go
+ * @date 6/12/22 4:48 PM
+ */
+public class CollectionsExerciseTest {
+
+    /**
+     * Collections.min(list))
+     * min(Collection c, Comparator comp)
+     *
+     * Collections.max(list)
+     * max(Collection c, Comparator comp)
+     *
+     * frequency(Collection c, object o)
+     */
+
+    @Test
+    public void learn_common_collections_operations() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+        list.add(6);
+        list.add(2);
+        System.out.println(Collections.max(list));
+        System.out.println(Collections.min(list));
+        int frequency = Collections.frequency(list, 2);
+        System.out.println(frequency);
+
+    }
+
+    /**
+     * synchronizedList()
+     */
+
+    @Test
+    public void learn_thread_safe_ArrayList() {
+        List<String> arrayList = new ArrayList<>();
+        List<String> synchronizedList = Collections.synchronizedList(arrayList);
+    }
+}
---
+++ Coding/collection/LinkedHashMapExerciseTest.java
@@ -0,0 +1,90 @@
+package com.chuwa.exercise.collection;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author b1go
+ * @date 6/12/22 4:48 PM
+ */
+public class LinkedHashMapExerciseTest {
+    /**
+     * e.g.
+     * HashMap<String, Integer> map = new LinkedHashMap<>();
+     *
+     * put(K key, V value)
+     * putIfAbsent(K key, V value)
+     * putAll(Map<? extends K, ? extends V> m)
+     *
+     * get(Object key)
+     * getOrDefault(Object key, V defaultValue)
+     *
+     * containsKey(Object key)
+     * containsValue(Object value)
+     *
+     * keySet()
+     * values()
+     * isEmpty()
+     */
+
+    @Test
+    public void learn_Inserting_And_Retrieving() {
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        map.put("a",1);
+        map.put("b",2);
+        map.putIfAbsent("a",2); //"a" is still 1
+        System.out.println(map);
+
+        Map<String, Integer> elementsToAdd = Map.of("c", 3, "d", 4);
+        map.putAll(elementsToAdd);
+        System.out.println(map);
+        System.out.println(map.get("c"));
+        System.out.println(map.getOrDefault("e", 0));
+
+        System.out.println(map.containsKey("a"));
+        System.out.println(map.containsValue(5));
+
+        Set<String> keyset = map.keySet();
+        List<Integer> values = new ArrayList<>(map.values());
+        System.out.println(keyset);
+        System.out.println(values);
+
+        System.out.println(map.isEmpty());
+    }
+
+    /**
+     * replace(K key, V oldValue, V newValue)
+     * replace(K key, V value)
+     * replaceAll(BiFunction<? super K, ? super V, ? extends V> function)
+     *
+     * remove(Object key)
+     * remove(Object key, Object value)
+     *
+     * compute(Key, BiFunction)
+     * computeIfAbsent(Key, Function)
+     * computeIfPresent(Key, BiFunction)
+     */
+    @Test
+    public void learn_Remove_Replacing_Updating() {
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        map.put("a",1);
+        map.put("b",2);
+        map.put("c",3);
+        map.put("d",4);
+        map.replace("a", 1, 5);
+        map.replace("a", 1, 6); //fail
+        map.replace("a", 5);
+        map.replaceAll((key, value)-> value*2);
+        System.out.println(map);
+
+        map.remove("a");
+        map.remove("b", 5); //fail
+
+        map.compute("c", (key, oldValue) -> oldValue * 2);
+        map.computeIfAbsent("c", key -> 3); //not change
+        map.computeIfAbsent("e", key -> 5);
+        map.computeIfPresent("b", (key, value) -> value * 2);
+        map.computeIfPresent("z", (key, value) -> 3); // fail
+    }
+}
