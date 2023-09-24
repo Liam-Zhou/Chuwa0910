57334fe40a0204c85e61c241143eb08f5bd8237f
cholt520 <wj4507657@gmail.com>
2023-09-24 00:14:26 -0700

    hw20

---
+++ Coding/ParkingLotServiceImpl.java
@@ -0,0 +1,17 @@
+public class ParkingLotServiceImpl implements ParkingLotService{
+    public double calculateFee(int totalHours,int perHourFee){
+        return 0;
+    }
+
+    public void callTowService(int parkingLotNumber,String TowCompany){
+
+    }
+
+    public String findManager(int parkingLotNumber){
+        return null;
+    }
+
+    public String findPhoneNumber(int parkingLotNumber){
+        return null
+    }
+}
