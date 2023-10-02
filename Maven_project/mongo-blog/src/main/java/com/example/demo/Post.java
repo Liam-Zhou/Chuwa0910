63e4ead1cc98054711fd3ea3edbb72b6976c910c
maomao <song2392823910@gmail.com>
2023-10-02 08:55:46 -0700

    yuhang hw7

---
+++ Maven_project/mongo-blog/src/main/java/com/example/demo/Post.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "posts")
+public class Post {
+
+    @Id
+    private String id;
+    private String title;
+    private String content;
+    // getters and setters
+}
---
+++ ShortQuestions/annotations.md
@@ -0,0 +1,14 @@
+@override: 
+重写
+
+@overload: 
+重载
+
+@Autowired：
+标记构造函数、字段或设置方法，以便通过Spring的依赖注入工具进行自动装配
+
+@Controller：
+表示一个类是一个“控制器”（例如，Web控制器）
+
+@Entity：
+标记一个类为JPA实体
