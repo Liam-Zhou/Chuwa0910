ebc3614dbfc9e66c69fa54ecd6c7ef7b971744a2
amber <amberlian403@gmail.com>
2023-10-10 23:13:15 -0700

    hw8

--- Coding/redbook-blog/src/main/java/com/chuwa/redbookblog/service/AuthorService.java
+++ Coding/redbook-blog/src/main/java/com/chuwa/redbookblog/service/AuthorService.java
@@ -3,7 +3,6 @@ import com.chuwa.redbookblog.payload.AuthorDto;
 import java.util.List;
 
 public interface AuthorService {
-
     List<AuthorDto> getAllAuthor();
     AuthorDto createAuthor(AuthorDto authorDto);
     AuthorDto getAuthorById(long id);
