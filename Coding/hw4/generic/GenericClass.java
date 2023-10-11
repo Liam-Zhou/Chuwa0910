503d2d35794c24856dfcccb863ae121b73f0a415
amber <amberlian403@gmail.com>
2023-10-10 18:45:23 -0700

    hw3

---
+++ Coding/hw4/generic/GenericClass.java
@@ -0,0 +1,13 @@
+
+public class GenericClass<T> {
+    // T -> Integer, User, Product
+    T obj;
+
+    public GenericClass(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return this.obj;
+    }
+}
