8b8f7ce31c2f268ea9f56230dd2bc97b20d4920f
apple zhang <zhangshe@usc.edu>
2023-10-12 14:59:51 -0700

    hw10

---
+++ Coding/tutorialtransaction/src/main/java/com/chuwa/tutorialtransaction/entity/Order.java
@@ -0,0 +1,37 @@
+package com.chuwa.tutorialtransaction.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private int totalQuantity;
+    private String orderTackingNumber;
+    private BigDecimal totalPrice;
+    private String status;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+    private Long shoppingCartId;
+}
