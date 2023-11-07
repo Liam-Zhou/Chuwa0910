89cafa5af80a57260699932331d1af230eeb367b
cholt520 <wj4507657@gmail.com>
2023-11-07 12:33:26 -0800

    hw50

--- Coding/redbook/src/main/java/com/chuwa/redbook/dao/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chuwa.redbook.dao;
-
-import com.chuwa.redbook.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author,Long> {
-}
