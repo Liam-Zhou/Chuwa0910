42569ad37184291d7f11fd4d6b23769eb4f71f25
sikicode <siqicao811@gmail.com>
2023-11-17 21:14:54 -0800

    reverse string intelliJ same file

--- Coding/hw0IntelliJ/src/main/java/com/example/hw0helloworld/Hw0HelloWorldApplication.java
+++ Coding/hw0IntelliJ/src/main/java/com/example/hw0helloworld/Hw0HelloWorldApplication.java
@@ -8,6 +8,18 @@ public class Hw0HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Hw0HelloWorldApplication.class, args);
+		System.out.println("Hello World!");
+
+		// Reverse string
+		String str = "abcdef", nstr = "";
+		char ch;
+		System.out.println("Original string: abcdef");
+		for(int i=0; i<str.length(); i++)
+		{
+			ch = str.charAt(i);
+			nstr = ch + nstr;
+		}
+		System.out.println("Reversed string: " + nstr);
 	}
 
 }
