a9165584383e2c0bbe684e48e4ae4dd7bde8b486
sikicode <siqicao811@gmail.com>
2023-11-17 20:45:01 -0800

    reset working folder

---
+++ Coding/hw0HelloWorld/src/main/java/com/example/hw0helloworld/Hw0HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw0helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw0HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw0HelloWorldApplication.class, args);
+	}
+
+}
