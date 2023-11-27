7729822a6609c7b23c7c21704deb74b5b0b7da9e
Ke Chen <kechen11257@gmail.com>
2023-11-27 16:09:24 -0700

    ke_chen/hw4-java8

---
+++ Coding/hw4-java8/src/main/java/kechen/exercise/Customer.java
@@ -0,0 +1,13 @@
+package kechen.exercise;
+
+public class Customer {
+    private ShoppingCart shoppingCart;
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+}
